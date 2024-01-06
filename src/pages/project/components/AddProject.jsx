fc693ad7bd76a37ac51312cf9ea835437cdf0d9b
NaiDang <Naid4243@gmail.com>
2024-01-06 17:47:29 +0700

    fix: add prject with managerId

--- src/pages/project/components/AddProject.jsx
+++ src/pages/project/components/AddProject.jsx
@@ -13,49 +13,52 @@ export const AddProject = () => {
   const [errorMessage, setErrorMessage] = useState(null);
   const [isModalVisible, setIsModalVisible] = useState(false);
   const [form] = Form.useForm();
-  // const [selectedMembers, setSelectedMembers] = useState([]);
-  // const [employeeOptions, setEmployeeOptions] = useState([]);
+  const [selectedMembers, setSelectedMembers] = useState([]);
+  const [memberOption, setMemberOptions] = useState([]);
+  const [selectedRoles, setSelectedRoles] = useState({});
   const [selectedManagers, setSelectedManagers] = useState([]);
   const [managerOptions, setManagerOptions] = useState([]);
 
   useEffect(() => {
     const fetchManagers = async () => {
       try {
-        await axios.get('http://localhost:3000/employee/managers').then(res => {
-        const data = res.data
+        const response = await axios.get('http://localhost:3000/employee/managers');
+        const data = response.data;
+        const managerData = data.map(manager => ({
+          id: manager.id,
+          name: manager.name,
+        }));
+        setManagerOptions(managerData);
+      } catch (error) {
+        console.error('Error fetching managers:', error);
+      }
+    };
 
-        setManagerOptions(data.map(manager => manager.id))
-        })
+    fetchManagers();
+  }, []);
+
+  useEffect(() => {
+    const fetchEmployees = async () => {
+      try {
+        const response = await axios.get('http://localhost:3000/employee');
+        const data = response.data.data;
+        const employeeData = data.map(employee => ({
+          id: employee.id,
+          name: employee.name,
+        }));
+        setMemberOptions(employeeData);
       } catch (error) {
         console.error('Error fetching employees:', error);
       }
     };
 
-    fetchManagers();
-
+    fetchEmployees();
   }, []);
 
-  // useEffect(() => {
-  //   const fetchEmployees = async () => {
-  //     try {
-  //       await axios.get('http://localhost:3000/employee').then(res => {
-  //       const data = res.data.data
-
-  //       setEmployeeOptions(data.map(employee => employee.id))
-  //       })
-  //     } catch (error) {
-  //       console.error('Error fetching employees:', error);
-  //     }
-  //   };
-
-  //   fetchEmployees();
-
-  // }, []);
 
   const showModal = () => {
     setIsModalVisible(true);
   };
-
   const handleOk = () => {
     form
       .validateFields()
@@ -89,7 +92,7 @@ export const AddProject = () => {
 
   return (
     <>
-      <Button type="primary" onClick={showModal} >
+      <Button type="primary" onClick={showModal}>
         Add Project
       </Button>
       <Modal
@@ -109,8 +112,8 @@ export const AddProject = () => {
         <Form
           form={form}
           labelCol={{
-            xs: { span: 24 },
-            sm: { span: 8 },
+            xs: { span: 6 },
+            sm: { span: 6},
           }}
           wrapperCol={{
             xs: { span: 24 },
@@ -118,91 +121,36 @@ export const AddProject = () => {
           }}
           layout="horizontal"
           onFinish={onFinish}
-          style={{
-            height: 300,
-          }}
         >
           <Row gutter={[16, 16]}>
             <Col xs={24} sm={12}>
               <Form.Item
+                placeholder="Enter project name"
                 label="Project name"
                 name="name"
                 rules={[{ required: true, message: 'Please enter Name!' }]}
               >
-                <Input />
+                <Input placeholder="Enter project name" />
               </Form.Item>
               <Form.Item
-                label="Manager ID"
+                label="Manager"
                 name="managerId"
-                rules={[{ required: true, message: 'Please enter Manager ID!' }]}
+                rules={[{ required: true, message: 'Please select a Manager!' }]}
               >
-                 <Select
-                  placeholder=""
+                <Select
+                  placeholder="Choose manager"
                   value={selectedManagers}
                   onChange={setSelectedManagers}
                   style={{ width: '100%' }}
                 >
                   {managerOptions.map((managerOption) => (
-                    <Option value={managerOption}/>
+                    <Option key={managerOption.id} value={managerOption.id}>
+                      {managerOption.name}
+                    </Option>
                   ))}
                 </Select>
               </Form.Item>
-              {/* <Form.Item
-                label="Members"
-                name="selectedMembers"
-                rules={[{ required: true, message: 'Please enter members' }]}
-              >
-
-                <Select
-                  mode="multiple"
-                  placeholder=""
-                  value={selectedMembers}
-                  onChange={setSelectedMembers}
-                  style={{ width: '100%' }}
-                >
-                  {employeeOptions.map((employeeOption) => (
-                    <Option value={employeeOption}/>
-                  ))}
-                </Select>
-              </Form.Item> */}
-              <Form.Item
-                label="Description"
-                name="description"
-                rules={[{ required: true, message: 'Please enter description' }]}
-              >
-                <TextArea rows={4} />
-              </Form.Item>
-            </Col>
-            <Col xs={24} sm={12}>
-              <Form.Item
-                label="Language/Framework"
-                name="langFrame"
-                rules={[{ required: true, message: 'Please enter Language/Framework!' }]}
-              >
-                 <Select  mode="multiple">
-                  <Option value="reactjs">ReactJs</Option>
-                  <Option value="htmlcss">HTML/CSS</Option>
-                  <Option value="nodejs">NodeJs</Option>
-                  <Option value="net">.NET</Option>
-                  <Option value="py">Python</Option>
-                  <Option value="java">Java</Option>
-                  <Option value="ang">AngularJs</Option>
-                </Select>
-              </Form.Item>
-              <Form.Item
-                label="Technology"
-                name="technology"
-                rules={[{ required: true, message: 'Please enter Technology!' }]}
-              >
-                <Select  mode="multiple">
-                  <Option value="git">Git</Option>
-                  <Option value="github">GitLab</Option>
-                  <Option value="gitlab">GitHub</Option>
-                  <Option value="aws">AWS CodeCommit</Option>
-                  <Option value="bitb">Bitbucket</Option>
-                  <Option value="blockc">Blockchain</Option>
-                </Select>
-              </Form.Item>
+              
               <Form.Item
                 label="Start Date"
                 name="startDate"
@@ -217,16 +165,59 @@ export const AddProject = () => {
               >
                 <DatePicker />
               </Form.Item>
+             
+            </Col>
+            <Col xs={24} sm={12}>
+              <Form.Item
+                label="LangFrame"
+                name="langFrame"
+                rules={[{ required: true, message: 'Please enter Language/Framework!' }]}
+              >
+                <Select mode="multiple" placeholder="Choose Languages and Framework">
+                  <Option value="reactjs">ReactJs</Option>
+                  <Option value="htmlcss">HTML/CSS</Option>
+                  <Option value="nodejs">NodeJs</Option>
+                  <Option value="net">.NET</Option>
+                  <Option value="py">Python</Option>
+                  <Option value="java">Java</Option>
+                  <Option value="ang">AngularJs</Option>
+                </Select>
+              </Form.Item>
+              <Form.Item
+                label="Technology"
+                name="technology"
+                rules={[{ required: true, message: 'Please enter Technology!' }]}
+              >
+                <Select mode="multiple" placeholder="Choose Technologies">
+                  <Option value="git">Git</Option>
+                  <Option value="github">GitLab</Option>
+                  <Option value="gitlab">GitHub</Option>
+                  <Option value="aws">AWS CodeCommit</Option>
+                  <Option value="bitb">Bitbucket</Option>
+                  <Option value="blockc">Blockchain</Option>
+                </Select>
+              </Form.Item>
+          
+              
+              
+              <Form.Item
+                label="Description"
+                name="description"
+                rules={[{ required: true, message: 'Please enter description' }]}
+              >
+                <TextArea rows={4} placeholder="Description of project" />
+              </Form.Item>
+             
             </Col>
           </Row>
+          <Row gutter={[2,2]}>
+            <Col span={24}>
+            </Col>
+            </Row>
         </Form>
       </Modal>
-      {successMessage && (
-        <div style={{ color: 'green' }}>{successMessage}</div>
-      )}
-      {errorMessage && (
-        <div style={{ color: 'red' }}>{errorMessage}</div>
-      )}
+      {successMessage && <div style={{ color: 'green' }}>{successMessage}</div>}
+      {errorMessage && <div style={{ color: 'red' }}>{errorMessage}</div>}
     </>
   );
 };
