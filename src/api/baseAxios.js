33941882abaab5fda8c3faff7a2d93a64c2876e4
Nguyen Thi Ut Vien <97380870+nguyenthiutvien@users.noreply.github.com>
2024-01-10 23:08:30 +0700

    update

--- src/api/baseAxios.js
+++ src/api/baseAxios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
+const BASE_URL = import.meta.env.VITE_BASE_URL_API;
 
 const baseAxios = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: BASE_URL,
     headers: {
         'Content-Type': 'application/json',
     },
