37c8e4f42ed98a358467792a0f66289c145789eb
Nguyen Thi Ut Vien <97380870+nguyenthiutvien@users.noreply.github.com>
2024-01-10 23:25:24 +0700

    update config

--- src/api/baseAxios.js
+++ src/api/baseAxios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 const BASE_URL = import.meta.env.VITE_BASE_URL_API;
 
 const baseAxios = axios.create({
-    baseURL: BASE_URL,
+    baseURL: BASE_URL, 
     headers: {
         'Content-Type': 'application/json',
     },
