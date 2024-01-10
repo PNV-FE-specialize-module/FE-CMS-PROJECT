b266c4fbf05ff5eef1897f85a8ef6f882b347345
Nguyen Thi Ut Vien <97380870+nguyenthiutvien@users.noreply.github.com>
2024-01-10 16:47:58 +0700

    update structure

--- src/api/baseAxios.js
+++ src/api/baseAxios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const baseAxios = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: 'https://nineams-diyv.onrender.com/',
     headers: {
         'Content-Type': 'application/json',
     },
